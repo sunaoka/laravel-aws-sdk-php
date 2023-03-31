f4dccd3f018b566361c01a98f9aeed5e57546a6f
sunaoka <sunaoka@pocari.org>
2023-03-31 11:54:19 +0900

    Move token under credentials

--- config/aws.php
+++ config/aws.php
@@ -18,8 +18,8 @@ return [
     'credentials' => [
         'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'token'  => env('AWS_SESSION_TOKEN'),
     ],
-    'token'       => env('AWS_SESSION_TOKEN') ?: null,
     'region'      => env('AWS_DEFAULT_REGION'),
     'version'     => 'latest',
     'endpoint'    => env('AWS_ENDPOINT'),
